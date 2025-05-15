276967194f4d27522640a73ba01c739e32bf5da5
NotDevinLe <devin.t.le@outlook.com>
2025-05-14 22:35:43 -0700

    change

--- api/inference.js
+++ api/inference.js
@@ -1,16 +1,15 @@
-import { InferenceClient } from "@huggingface/inference";
-
-const client = new InferenceClient("hf_xxxxxxxxxxxxxxxxxxxxxxxx");
-
-const chatCompletion = await client.chatCompletion({
-    provider: "novita",
-    model: "deepseek-ai/DeepSeek-V3-0324",
-    messages: [
-        {
-            role: "user",
-            content: "How many 'G's in 'huggingface'?",
-        },
-    ],
-});
-
-console.log(chatCompletion.choices[0].message);
+export default async function handler(req, res) {
+    const { input } = req.body;
+  
+    const hfRes = await fetch('https://api-inference.huggingface.co/models/deepseek-ai/DeepSeek-V3-0324', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer ${process.env.hf_api_key}',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ inputs: input }),
+    });
+  
+    const data = await hfRes.json();
+    res.status(200).json(data);
+  }
